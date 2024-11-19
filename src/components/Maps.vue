2f3de32d1a9d4ffea8d0e7795c52c09be2c2ddba
Ulysses <tranquilinoulysses9@gmail.com>
2024-11-19 13:18:15 +0800

    add responsinve

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,49 +1,26 @@
-<template>
-  <div class="bg-primary q-mt-xl q-pa-lg">
-    <div class="row">
-      <div id="q-app">
-        <div class="q-pa-md">
-          <q-carousel animated v-model="slide" arrows navigation infinite>
-            <!-- Carousel Slides -->
-            <q-carousel-slide
-              :name="1"
-              img-src="https://cdn.quasar.dev/img/mountains.jpg"
-            ></q-carousel-slide>
-            <q-carousel-slide
-              :name="2"
-              img-src="https://cdn.quasar.dev/img/parallax1.jpg"
-            ></q-carousel-slide>
-            <q-carousel-slide
-              :name="3"
-              img-src="https://cdn.quasar.dev/img/parallax2.jpg"
-            ></q-carousel-slide>
-            <q-carousel-slide
-              :name="4"
-              img-src="https://cdn.quasar.dev/img/quasar.jpg"
-            ></q-carousel-slide>
-          </q-carousel>
-        </div>
+<template class="template-map">
+  <div class="bg-primary q-pt-xl">
+    <div class="bg-primary q-mt-xl q-pa-lg q-pa-lg-xl all-maps">
+      <div class="row">
+        <h1 class="text-title text-white main-map-title">MAPS</h1>
+        <P class="text-white text-left main-map-desc" style="font-size: 16px">
+          Each map is a playground to showcase your creative thinking.
+          Purpose-built for team strategies, spectacular plays, and clutch
+          moments. Make the play others will imitate for years to come
+        </P>
       </div>
-    </div>
-    <div class="row">
-      <h1 class="text-title text-white">MAPS</h1>
-      <P class="text-white text-left" style="font-size: 16px">
-        Each map is a playground to showcase your creative thinking.
-        Purpose-built for team strategies, spectacular plays, and clutch
-        moments. Make the play others will imitate for years to come
-      </P>
-    </div>
 
-    <div v-for="map in data" :key="map.uuid" class="row maps-container">
-      <div class="q-mb-lg map-container">
-        <div class="flex justify-start row">
-          <h1 class="map-title q-mb-xs q-mt-xs col-12 text-left">
-            {{ map.displayName }}
-          </h1>
-          <p class="map-coordinates">{{ map.coordinates }}</p>
-        </div>
-        <div class="flex justify-center items-center">
-          <img :src="map.splash" :alt="map.displayName" class="map-img" />
+      <div v-for="map in data" :key="map.uuid" class="row maps-container">
+        <div class="q-mb-lg map-container">
+          <div class="flex justify-start row">
+            <h1 class="map-title q-mb-xs q-mt-xs col-12 text-left">
+              {{ map.displayName }}
+            </h1>
+            <p class="map-coordinates">{{ map.coordinates }}</p>
+          </div>
+          <div class="flex justify-center items-center">
+            <img :src="map.splash" :alt="map.displayName" class="map-img" />
+          </div>
         </div>
       </div>
     </div>
@@ -81,7 +58,6 @@ onMounted(() => {
 
 <style scoped>
 .carousel-container {
-  border: 2px solid white;
   width: 100%;
   height: 200px;
 }
@@ -104,10 +80,41 @@ onMounted(() => {
   font-size: 15px;
   color: white;
 }
+@media (min-width: 768px) {
+  .main-map-title {
+    font-size: 100px;
+    margin-bottom: 20px;
+  }
 
-.maps-container {
+  .main-map-desc {
+    font-size: 30px;
+    margin-bottom: 70px;
+  }
+  .map-title {
+    font-size: 80px;
+  }
+
+  .map-coordinates {
+    font-size: 20px;
+    margin-bottom: 40px;
+  }
+
+  .map-container {
+    margin-bottom: 100px;
+  }
 }
 
-.map-container {
+@media (min-width: 992px) {
+  .all-maps {
+    padding: 45px;
+  }
+}
+
+@media (min-width: 1250px) {
+  .all-maps {
+    width: 1200px;
+    margin: auto;
+    padding: 0;
+  }
 }
 </style>
